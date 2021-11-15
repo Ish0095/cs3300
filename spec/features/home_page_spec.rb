dac891debb14ede0ee12edfa50940c2b1e1e40ee
root <isingh@uccs.edu>
2021-11-15 20:46:34 +0000

    Init

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,8 +1,8 @@
-require 'rails_helper'
+#require 'rails_helper'
 
-RSpec.feature "HomePages", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
-end
+#RSpec.feature "HomePages", type: :feature do
+ # pending "add some scenarios (or delete) #{__FILE__}"
+#end
 
 
 require "rails_helper"
--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -1,8 +1,8 @@
-require 'rails_helper'
+#require 'rails_helper'
 
-RSpec.feature "Projects", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
-end
+#RSpec.feature "Projects", type: :feature do
+#  pending "add some scenarios (or delete) #{__FILE__}"
+#end
 
 
 require 'rails_helper'
